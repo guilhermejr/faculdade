d4dbb521f7cd72090059a0a038a338e85c5584a6
Guilherme Jr <falecom@guilhermejr.net>
2021-10-26 17:46:38 -0300

    feat: adicionado comentários

--- main.c
+++ main.c
@@ -5,141 +5,149 @@
 
 void limparTela()
 {
-	system("cls");
+	system("cls"); // Limpa a tela
 }
 
 void dadosParque()
 {
+	// Bloco com os dados do parque
 	printf("PARQUE DOS ENGENHEIROS\n");
 	printf("Rua das pedras, 666 - Jardim Botânico\n");
 	printf("Salvador - Bahia\n\n");
 }
 
-void ingressoDuracao()
+void valorDuracao()
 {
-	printf("INGRESSO: R$ 5,00\n");
+	// Bloco com o valor e duração do ingresso
+	printf("VALOR: R$ 5,00\n");
 	printf("DURAÇÃO: 10 minutos\n\n");
 }
 
 void splash()
 {
-	limparTela();
-	dadosParque();
-	ingressoDuracao();
+	limparTela(); // Limpa a tela
+	dadosParque(); // Dados do parque
+	valorDuracao(); // Valor e duração do ingresso
 	printf("PRESSIONE UMA TECLA PARA COMPRAR INGRESSO\n\n");
-	getch();
+	getch(); // Aguarda pressionar qualquer tecla
 }
 
 int quantidadeIngressos()
 {
 	
-	int quantidade;
+	int quantidade; // Variável quantidade
 	
-	limparTela();
-	ingressoDuracao();
+	limparTela(); // Limpa a tela
+	valorDuracao(); // Dados do parque
 	printf("Deseja comprar quantos ingressos? ");
-	scanf("%d", &quantidade);
+	scanf("%d", &quantidade); // Quantidade de ingressos
 	
-	return quantidade;
+	return quantidade; // Retorno da quantidade
 }
 
 void totais(int quantidade)
 {
-	limparTela();
+	limparTela(); // Limpa a tela
+	
+	// Total de ingresso e valor
 	printf("Total de ingressos: %d\n", quantidade);
 	printf("Valor total: R$ %d,00\n\n", (quantidade * 5));
 }
 
 char formaPagamento(int quantidade)
 {
-	char forma;
+	char forma; // Varável forma
 	
-	totais(quantidade);
+	totais(quantidade); // Mostra mensagem de ingresso e valor
+	
+	// Menu de opções
 	printf("Escolha forma de pagamento:\n");
 	printf("1 - Débito\n");
 	printf("2 - Crédito\n");
-	forma = getch();
+	forma = getch(); // Pega opção escolhida
 	
-	return forma;
+	return forma; // Retorno da forma
 }
 
 void formaDePagamento(char forma)
 {
-	if (forma == '1') {
+	if (forma == '1') 
+	{ // Se opção escolhida for 1
 		printf("Forma de pagamento: Débito\n\n");
 	}	
 	
 	if (forma == '2')
-	{
+	{ // Se opção escolhida for 2
 		printf("Forma de pagamento: Crédito\n\n");
 	}
 }
 
 char confirmacao(int quantidade, char forma)
 {
-	char confirmar;
+	char confirmar; // Variável confirmar
 	
-	totais(quantidade);
+	totais(quantidade); // Mostra mensagem de ingresso e valor
 	
-	formaDePagamento(forma);
+	formaDePagamento(forma); // Mostra forma de pagamento
 	
+	// Menu de opções
 	printf("Confirma informações?\n");
 	printf("1 - Sim\n");
 	printf("2 - Não\n");
-	confirmar = getch();
+	confirmar = getch(); // Pega opção escolhida
 
-	return confirmar;
+	return confirmar; // Retorno de confirmar
 	
 }
 
 void barra(int segundos)
 {
-	int i;
+	int i; // Variável de controle
 	
 	for (i = 0; i < segundos; i++) 
 	{
-		Sleep(1000);
-		printf("#");
+		Sleep(1000); // Aguarda 1 segundo
+		printf("#"); // Imprime #
 	}
-	printf("\n");
+	printf("\n"); // Pula uma linha
 }
 
 void cobranca()
 {
-	limparTela();
+	limparTela(); // Limpa a tela
 	printf("Siga as instrções da máquina de cartão.\n\n");
-	barra(15);
+	barra(15); // Mostra a barra
 }
 
 void impressao(int quantidade, char forma)
 {
-	dadosParque();
-	totais(quantidade);
-	formaDePagamento(forma);
+	dadosParque(); // Dados do parque
+	totais(quantidade); // Mostra mensagem de ingresso e valor
+	formaDePagamento(forma); // Mostra forma de pagamento
 	printf("AGUARDE IMPRESSÃO DO INGRESSO.\n\n");
-	barra(12);
-	
+	barra(12); // Mostra a barra
 }
 
 int main() 
 {
 	
-	int quantidade;
-	char forma, confirmar;
+	int quantidade; // Variável quantidade
+	char forma, confirmar; // Variáveis forma e confirmar
 	
-	setlocale(LC_ALL, "Portuguese");
+	setlocale(LC_ALL, "Portuguese"); // Permite que seja impresso acentos
+	
 	do {
-		splash();
-		quantidade = quantidadeIngressos();
-		forma = formaPagamento(quantidade);
-		confirmar = confirmacao(quantidade, forma);
+		splash(); // Mostra tela inicial
+		quantidade = quantidadeIngressos(); // Mostra tela para selecionar quantidade de ingressos
+		forma = formaPagamento(quantidade); // Mostra tela para selecionar forma de pagamento
+		confirmar = confirmacao(quantidade, forma); // Mostra tela de confirmação
 		
 		if (confirmar == '2') {
-			continue;
+			continue; // Volta para o início do loop
 		}
 		
-		cobranca();
-		impressao(quantidade, forma);
+		cobranca(); // Mostra tela de cobrnaça
+		impressao(quantidade, forma); // Mostra tela de impressão
 		
 	} while (0 == 0);
 	
